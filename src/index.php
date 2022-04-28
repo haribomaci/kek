aa25cf9289fb57d762fd538a4ea12a14f4acb480
toth julia <t.julia982@gmail.com>
2022-04-28 09:13:57 +0200

    lekeres mukodik

--- src/index.php
+++ src/index.php
@@ -6,5 +6,21 @@ if($con -> connect_error){
     die("Hiba! A kapcsolódás sikertelen");
 }
 
-echo "ok";
+//echo "ok";
+
+$sql = <<<EOT
+select * from dolgozok
+
+EOT;
+
+if($result = $con->query($sql)){
+    echo "lekérdezés ok";
+}
+else{
+    echo "Hiba! a lekérdezés sikertelen";
+}
+
+while($row = $result->fetch_assoc()){
+    echo $row['nev'];
+}//kiszedjük az adatokat belőle, és hogy milyen módon
 $con->close();
