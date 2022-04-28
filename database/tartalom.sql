479d9bbf8622aab9ee489d26db8467b9300cd791
toth julia <t.julia982@gmail.com>
2022-04-28 08:50:18 +0200

    adatbazishoz tartalom hozzaadva

---
+++ database/tartalom.sql
@@ -0,0 +1,9 @@
+insert into dolgozok
+--milyen mezőket akarunk megadni
+(nev, telepules, fizetes)
+values
+('Tangó Béla', 'Szeged', 385),
+('Okos Árpád', 'Szolnok', 567),
+('Lehel Károly', 'Szeged', 332),
+('Terem Irén', 'Szolnok', 978),
+('Fakó Lajos', 'Szeged', 432);
